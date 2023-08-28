c5a23bda5377b12c8b32a05a23870a555acd6d9d
Ogewo <71373828+Ogewo@users.noreply.github.com>
2023-08-28 14:27:53 +0300

    updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,39 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {FaSearch,FaUser,FaShoppingBag} from 'react-icons/fa'
 import { menuItems } from './menu'
 import './NavbarStyles.css'
-function Navbar() {
-   return(
-    <nav>         
-        {/* <div>
-        <ul>
-            <li><a href='#'>Home</a></li>
-            <li><a href='#'>Men</a></li>
-            <li><a href='#'>Women</a></li>
-            <li><a href='#'>Pages</a></li>
-            <li><a href='#'>Blog</a></li>
-         </ul>
-        </div>         
-         <h2>Salehub</h2>
-         <div><FaSearch className='icon'/><a href='#'>Search</a></div> 
-         <div id='icons'>
-         <div><FaUser className='icon'/><a href='#'>Account</a></div>
-         <div><FaShoppingBag className='icon'/><a href='#'>My Cart</a></div>
-         </div> */}
 
+function Navbar() {
+    const [showForm, setShowForm] = useState(false)
+    const handleNavLinkHover = () => {
+        setShowForm(true)
+    }
+    const handleNavLeave = () => {
+        setShowForm(false)
+    }
+
+   return(
+    <nav>
        {menuItems.map((item, index) => (
         <div key={index} className='nav-item'>
             {item.title}
-            {item.subMenu && (
-                <ul className='sub-menu'>
-                    {item.subMenu.map((subItem, subIndex) => (
-                        <li key={subIndex} className='sub-item'>
-                            <strong>{subItem.title }</strong>
-                            {subItem.subtitle}
-                        </li>
-                    ))}
-                </ul>
-            )}
+                {item.subMenu && (
+                    <ul className={`sub-menu ${showForm ? 'active' : ''}`}>
+                        {item.subMenu.map((subItem, subIndex) => (
+                            <div className='sub-item'><li key={subIndex} className=''>
+                                <strong>{subItem.title}</strong>
+                                <div className='sub-items'>{subItem.subtitle}</div>
+                            </li></div>
+                        ))}
+                    </ul>
+                )}
         </div>
        ))}
     </nav>
