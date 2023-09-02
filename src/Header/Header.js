b080ac6e9d8d79479d403ba752315ecbbccea100
Ogewo <71373828+Ogewo@users.noreply.github.com>
2023-09-02 23:30:30 +0300

    updated

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,14 +1,45 @@
-import React from 'react'
+import React, {useEffect, useRef} from 'react'
 import { FaPaperPlane, FaBars} from 'react-icons/fa'
+
 const Header = () => {
+
+  const headerRef = useRef(null)
+
+  const stickyHeaderFunc = () =>{
+    window.addEventListener('scroll', ()=>{
+      if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80){
+        headerRef.current.classList.add('sticky__header')
+      }else{
+        headerRef.current.classList.remove('sticky__header')
+      }
+    })
+  }
+
+  useEffect(() =>{
+    stickyHeaderFunc()
+    return window.removeEventListener('scroll', stickyHeaderFunc)
+  }, [])
+
+  const handleClick = e =>{
+    e.preventDefault()
+
+    const targetAttr = e.target.getAttribute('href')
+    const location = document.querySelector(targetAttr).offSetTop
+
+    window.scrollTo({
+      top: location = 80,
+      left:  0
+    })
+  }
+
   return (
     <>
      <header className='w-full h-[80px] flex
      items-center px-[200px]'>
      <div className='container'>
-        <div className='flex items-center justify-between'>
+        <div className='hidden md:flex items-center md:justify-between border'>
           {/* ===========Logo============ */}
-          <div className='flex items-center gap-2'>
+          <div className='flex items-center md:gap-2'>
           <span className='w-[35px] h-[35px] bg-primaryColor text-white text-[18px] font-[500] rounded-full flex items-center justify-center'>E</span>
           <div>
             <h2 className='text-xl font-[700]'>Eli</h2>
@@ -18,11 +49,11 @@ const Header = () => {
           {/* =========Logo End========= */}
           {/* =========menu start========= */}
           <div className='menu'>
-            <ul className='flex items-center gap-[70px]'>
-              <li><a className='text-smallTextColor font-[600]' href='#'>About</a></li>
-              <li><a className='text-smallTextColor font-[600]' href='#'>Services</a></li>
-              <li><a className='text-smallTextColor font-[600]' href='#'>Portfolio</a></li>
-              <li><a className='text-smallTextColor font-[600]' href='#'>Contact</a></li>
+            <ul className='flex items-center gap-[40px] md:gap-[70px]'>
+              <li><a onClick={handleClick} className='text-smallTextColor font-[600]' href='#'>About</a></li>
+              <li><a onClick={handleClick} className='text-smallTextColor font-[600]' href='#'>Services</a></li>
+              <li><a onClick={handleClick} className='text-smallTextColor font-[600]' href='#'>Portfolio</a></li>
+              <li><a onClick={handleClick} className='text-smallTextColor font-[600]' href='#'>Contact</a></li>
             </ul>
           </div>
           {/* =========Logo end========= */}
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { FaEnvelope,FaTwitter,FaYoutube,FaLinkedin,FaGithub } from 'react-icons/fa'
+
 export const Footer = () => {
+       const year = new Date().getFullYear()
   return (
     <>
-     <footer className='flex items-center justify-center bg-[#12141e] pt-12'>
+     <footer className='bg-[#12141e] pt-12'>
       {/*============footer top */}
-      <div className='container'>
-        <div className='sm:flex items-center  md:gap-8'>
+      <div className='container px-40'>
+        <div className='sm:flex items-center justify-center md:gap-8'>
           <div className='w-full sm:w-1/2'>
             <h2 className='text-[26px] leading-10 text-white font-[600] mb-5 md:text-[1.2rem]'>Do you want to make beautiful products?</h2>
             <a href='#'>
@@ -15,31 +17,31 @@ export const Footer = () => {
                     </button>
             </a>
           </div>
-          <div className='w-full sm:w-1/2'>
-            <p className='text-gray-300 leading-7 mt-4 sm:mt-0'>
+          <div className='bg-red-100 w-full sm:w-1/2'>
+            <p className='text-center text-gray-300 leading-7 mt-4 sm:mt-0'>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
             Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, 
             temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!
             </p>
             <div className='flex items-center gap-4 flex-wrap md:gap-8 mt-10'>
               <span className='text-gray-300 font-[600] text-[15px]'>Follow Me</span>
-              <span className='w-35px] h-[35px] bg-[#2b2d33] p-1 rounded-[50px] cursor-pointer text-center'>
+              <span>
                 <a href='#' className='text-gray-300 font-[500] text-[18px]'><FaGithub /></a>
               </span>
-              <span className='w-35px] h-[35px] bg-[#2b2d33] p-1 rounded-[50px] cursor-pointer text-center'>
+              <span>
                 <a href='#' className='text-gray-300 font-[500] text-[18px]'><FaTwitter /></a>
               </span>
-              <span className='w-35px] h-[35px] bg-[2b2d33] p-1 rounded-[50px] cursor-pointer text-center'>
+              <span>
                 <a href='#' className='text-gray-300 font-[500] text-[18px]'><FaLinkedin/></a>
               </span>
-              <span className='w-35px] h-[35px] bg-[#2b2d33] p-1 rounded-[50px] cursor-pointer text-center'>
+              <span>
                 <a href='#' className='text-gray-300 font-[500] text-[18px]'><FaYoutube /></a>
               </span>             
             </div>
           </div>
           </div>
           <div>
-            <ul className='flex items-center gap-[70px]'>
+            <ul className='flex items-center justify-center gap-[70px] pt-20'>
               <li><a className='text-white font-[600]' href='#'>About</a></li>
               <li><a className='text-white font-[600]' href='#'>Portfolio</a></li>
               <li><a className='text-white font-[600]' href='#'>Services</a></li>
@@ -48,6 +50,25 @@ export const Footer = () => {
           </div>
       </div>
       {/*============footer top end ========*/}
+      {/*============footer bottom ========*/}
+      <div className='bg-[#1b1e29] py-3 mt-14 px-40'>
+        <div className='container'>
+          <div className='flex items-center justify-center sm:justify-between'>
+            <div className='hidden sm:block'>
+              <div className='flex items-center gap-[10px]'>
+                <span className='w-[35px] h-[35px] rounded-full bg-[#2b2d33] text-white font-[500] text-[18px] flex items-center justify-center'>E</span>
+                <div className='leading-[20px]'>
+                  <h2 className='text-gray-200 font-[500] text-[18px]'>Eli</h2>
+                  <p className='text-gray-400 text-[14px] font-[500]'>personal</p>
+                </div>
+              </div>
+            </div>
+            <div>
+              <p className='text-gray-400 text-[14px]'>Copyright {year} by Eli - All rights reserved.</p>
+            </div>
+          </div>
+        </div>
+      </div>
      </footer>
     </>
   )
